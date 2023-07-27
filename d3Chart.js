60d1d4cb5431dfa84642cee2c9ce965e61bf969e
Jeremy Ahn <jcahn2@illinois.edu>
2023-07-27 00:23:50 -0500

    adding transitions2

--- d3Chart.js
+++ d3Chart.js
@@ -2,12 +2,12 @@ function chart(container, data){
     ticker = data
                 
     // Declare the chart dimensions and margins.
-    const width = 200;
-    const height = 200;
-    const marginTop = 50;
-    const marginRight = 50;
-    const marginBottom = 50;
-    const marginLeft = 50;
+    const width = 928;
+    const height = 600;
+    const marginTop = 20;
+    const marginRight = 30;
+    const marginBottom = 30;
+    const marginLeft = 40;
 
     // Declare the positional encodings.
     var x = d3.scaleBand()
@@ -24,82 +24,73 @@ function chart(container, data){
     // Create the SVG container.
     var svg = d3.select(container).append("svg")
         .attr("viewBox", [0, 0, width, height]);
+}
 
-    
-
-
-    // Update function to transition
-    function update(ticker) {
-        // Append the axes.
-        svg.append("g")
-            .attr("transform", `translate(0,${height - marginBottom})`)
-            .call(d3.axisBottom(x)
-            .tickValues(d3.utcMonday
-                .every(width > 720 ? 1 : 2)
-                .range(ticker.at(0).Date, ticker.at(-1).Date))
-            .tickFormat(d3.utcFormat("%-m/%-Y")))
-            .call(g => g.select(".domain").remove())
-            .transition()
-            .duration(2000);
-
-        svg.append("g")
-            .attr("transform", `translate(${marginLeft},0)`)
-            .call(d3.axisLeft(y)
-            .tickFormat(d3.format("$~f"))
-            .tickValues(d3.scaleLinear().domain(y.domain()).ticks()))
-            .call(g => g.selectAll(".tick line").clone()
-            .attr("stroke-opacity", 0.2)
-            .attr("x2", (width - marginLeft - marginRight)))
-            .call(g => g.select(".domain").remove())
-            .transition()
-            .duration(2000);
-
-        
-        // Create a group for each day of data, and append two lines to it.
-        var g = svg.append("g")
-            .attr("stroke-linecap", "round")
-            .attr("stroke", "black")
-        .selectAll("g")
-        .data(ticker)
-        .join("g")
-            .attr("transform", d => `translate(${x(d3.utcDay(d.Date))},0)`)
+// Update function to transition
+function update(ticker) {
+    // Append the axes.
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+        .tickValues(d3.utcMonday
+            .every(width > 720 ? 1 : 2)
+            .range(ticker.at(0).Date, ticker.at(-1).Date))
+        .tickFormat(d3.utcFormat("%-m/%-Y")))
+        .call(g => g.select(".domain").remove())
         .transition()
         .duration(2000);
 
-        g   .enter()
-            .append("line")
-            .merge(g)
-            .transition()
-            .duration(2000)
-            .attr("y1", d => y(d.Low))
-            .attr("y2", d => y(d.High));
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y)
+        .tickFormat(d3.format("$~f"))
+        .tickValues(d3.scaleLinear().domain(y.domain()).ticks()))
+        .call(g => g.selectAll(".tick line").clone()
+        .attr("stroke-opacity", 0.2)
+        .attr("x2", (width - marginLeft - marginRight)))
+        .call(g => g.select(".domain").remove())
+        .transition()
+        .duration(2000);
 
-        g   .enter()
-            .append("line")
-            .merge(g)
-            .transition()
-            .duration(2000)
-            .attr("y1", d => y(d.Open))
-            .attr("y2", d => y(d.Close))
-            .attr("stroke-width", x.bandwidth())
-            .attr("stroke", d => d.Open > d.Close ? d3.schemeSet1[0]
-                : d.Close > d.Open ? d3.schemeSet1[2]
-                : d3.schemeSet1[8]);
+    
+    // Create a group for each day of data, and append two lines to it.
+    var g = svg.append("g")
+        .attr("stroke-linecap", "round")
+        .attr("stroke", "black")
+    .selectAll("g")
+    .data(ticker)
+    .join("g")
+        .attr("transform", d => `translate(${x(d3.utcDay(d.Date))},0)`)
+    .transition()
+    .duration(2000);
 
-        // Append a title (tooltip).
-        const formatDate = d3.utcFormat("%B %-d, %Y");
-        const formatValue = d3.format(".2f");
-        const formatChange = ((f) => (y0, y1) => f((y1 - y0) / y0))(d3.format("+.2%"));
+    g.append("line")
+        .transition()
+        .duration(2000)
+        .attr("y1", d => y(d.Low))
+        .attr("y2", d => y(d.High));
 
-        g.append("title")
-            .text(d => `${formatDate(d.Date)}
-        Open: ${formatValue(d.Open)}
-        Close: ${formatValue(d.Close)} (${formatChange(d.Open, d.Close)})
-        Low: ${formatValue(d.Low)}
-        High: ${formatValue(d.High)}`);
-    }
+    g.append("line")
+        .transition()
+        .duration(2000)
+        .attr("y1", d => y(d.Open))
+        .attr("y2", d => y(d.Close))
+        .attr("stroke-width", x.bandwidth())
+        .attr("stroke", d => d.Open > d.Close ? d3.schemeSet1[0]
+            : d.Close > d.Open ? d3.schemeSet1[2]
+            : d3.schemeSet1[8]);
 
-    update(ticker)
+    // Append a title (tooltip).
+    const formatDate = d3.utcFormat("%B %-d, %Y");
+    const formatValue = d3.format(".2f");
+    const formatChange = ((f) => (y0, y1) => f((y1 - y0) / y0))(d3.format("+.2%"));
+
+    g.append("title")
+        .text(d => `${formatDate(d.Date)}
+    Open: ${formatValue(d.Open)}
+    Close: ${formatValue(d.Close)} (${formatChange(d.Open, d.Close)})
+    Low: ${formatValue(d.Low)}
+    High: ${formatValue(d.High)}`);
 }
 
 
