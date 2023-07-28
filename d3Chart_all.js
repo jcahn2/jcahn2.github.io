2bcdaa8101b4b7b09ad90b97c82f8fa51a61a00b
Jeremy Ahn <jcahn2@illinois.edu>
2023-07-28 16:54:04 -0500

    update overview4

--- d3Chart_all.js
+++ d3Chart_all.js
@@ -13,20 +13,39 @@ function chart_all(container, data){
             .domain(d3.extent(ticker, d => d3.utcDay(d.Date)))
             .range([marginLeft, width - marginRight]);
 
-    const y = d3.scaleLog()
+    const y = d3.scaleLinear()
         .domain([d3.min(ticker, d => d.Low), d3.max(ticker, d => d.High)])
         .rangeRound([height - marginBottom, marginTop]);
 
     // Create the SVG container.
     const svg = d3.select(container).append("svg")
         .attr("viewBox", [0, 0, width, height]);
+    
+    // SELECTION BUTTON options and colors
+    var allGroup = ["Open", "Close", "Low", "High"]
+
+    // add options to button
+    d3.select("#selectButton")
+        .selectAll("myOptions")
+        .data(allGroup)
+        .enter()
+        .append("option")
+        .text(function(d) {return d;})
+        .attr("value", function(d) {return d;});
+
+    // color scale, one for each group
+    var myColor = d3.scaleOrdinal()
+        .domain(allGroup)
+        .range(d3.schemeSet2);
+
 
     // Append the axes.
     svg.append("g")
         .attr("transform", `translate(0,${height - marginBottom})`)
         .call(d3.axisBottom(x)
         .tickValues(d3.utcMonth
-            .range(+ticker.at(0).Date - 5, ticker.at(-1).Date), 3)
+            .every(4)
+            .range(+ticker.at(0).Date - 5, ticker.at(-1).Date))
         .tickFormat(d3.utcFormat("%-m/%-Y")))
         .call(g => g.select(".domain").remove())
         .selectAll("text")
@@ -61,16 +80,17 @@ function chart_all(container, data){
     
     // Create a group for each day of data
     const g = svg.append("g")
-        .selectAll("g")
-        .data(ticker)
         .append("path")
+        .datum(ticker)
         .attr("d", d3.line()
+            .curve(d3.curveLinear)
             .x(d => x(d3.utcDay(d.Date)))
-            .y(d => +d.Open)
+            .y(d => y(d.Open))
         )
-            .attr("stroke", d3.schemeSet2[0])
-            .style("stroke-width",4)
+            .attr("stroke", function(d) { return myColor("Open");})
+            .style("stroke-width",2)
             .style("fill", "none")
+        .data(ticker)
         .on("mouseover", function(d){
             Tooltip.style("opacity",1)
         })
@@ -87,6 +107,35 @@ function chart_all(container, data){
         .on("mouseleave", function(d){
             Tooltip.style("opacity",0)
         });
+
+    // function to update chart
+    function update(selectedGroup) {
+        // Create new data with the selection?
+        var dataFilter = ticker.map(function(d){return {Date: d.Date, Value:d[selectedGroup]} })
+
+        // Give these new data to update line
+        g
+            .datum(ticker)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+                .x(function(d) { return x(d3.utcDay(d.Date)) })
+                .y(function(d) { return y(d[selectedGroup]) })
+            )
+            .attr("stroke", function(d){ return myColor(selectedGroup) });
+        }
+
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+        // run the updateChart function with this selected option
+        update(selectedOption)
+    
+    });
+
+    
 
 
 }
